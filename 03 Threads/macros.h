85b8d43199d1f9dd65d678276083dbc9abe45f84
RadekMocek <radek.mocek@tul.cz>
2022-11-01 12:35:28 +0100

    CV03

---
+++ 03 Threads/macros.h
@@ -0,0 +1,14 @@
+#define text1 "TEXT ONE"
+#define delay1 1s
+#define line1 1
+#define color1 LCD_COLOR_RED
+
+#define text2 "TEXT TWO"
+#define delay2 2s
+#define line2 2
+#define color2 LCD_COLOR_YELLOW
+
+#define text3 "TEXT THREE"
+#define delay3 3s
+#define line3 3
+#define color3 LCD_COLOR_GREEN
---
+++ 03 Threads/main.cpp
@@ -0,0 +1,62 @@
+#include "mbed.h"
+#include "LCD_DISCO_F469NI.h"
+ 
+#include <macros.h>
+
+LCD_DISCO_F469NI lcd;
+
+Thread thread1;
+Thread thread2;
+Thread thread3;
+
+bool txt1visible;
+bool txt2visible;
+bool txt3visible;
+
+void txt1_thread()
+{
+    while (true) {
+        if (!txt1visible) {
+            lcd.SetTextColor(color1);
+            lcd.DisplayStringAtLine(line1, (uint8_t *)text1);
+        }
+        else lcd.ClearStringLine(line1);
+        txt1visible = !txt1visible;
+        ThisThread::sleep_for(delay1);
+    }
+}
+void txt2_thread()
+{
+    while (true) {
+        if (!txt2visible) {
+            lcd.SetTextColor(color2);
+            lcd.DisplayStringAtLine(line2, (uint8_t *)text2);
+        }
+        else lcd.ClearStringLine(line2);
+        txt2visible = !txt2visible;
+        ThisThread::sleep_for(delay2);
+    }
+}
+void txt3_thread()
+{
+    while (true) {
+        if (!txt3visible) {
+            lcd.SetTextColor(color3);
+            lcd.DisplayStringAtLine(line3, (uint8_t *)text3);
+        }
+        else lcd.ClearStringLine(line3);
+        txt3visible = !txt3visible;
+        ThisThread::sleep_for(delay3);
+    }
+}
+int main()
+{
+    txt1visible = false;
+    txt2visible = false;
+    txt3visible = false;
+
+    thread1.start(txt1_thread);
+    thread2.start(txt2_thread);
+    thread3.start(txt3_thread);
+}
+
